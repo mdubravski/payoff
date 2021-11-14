f78ceb67ff7956eeb6502df73b23cf15a34a7da1
mdubravski <mdubravski@gmail.com>
2021-11-14 09:12:43 -0500

    add responsive nav

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// load particles.js
 particlesJS.load('particles-js', 'particles.json', ()=>{
-    console.log('JSON loaded');
+    console.log('particles.js loaded');
 });
+
+// nav functionality
+const menu = document.querySelector('.menu');
+const content = document.querySelector('.wrapper');
+const hamburger = document.querySelector('.hamburger');
+const bars = hamburger.querySelectorAll('span');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    content.classList.toggle('shift');
+    
+    for(let b of bars){
+        b.classList.toggle('change');
+    }
+});
+
+
