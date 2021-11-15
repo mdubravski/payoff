ed7286f1541049f5cc9fbb58dfbf61126990377f
mdubravski <mdubravski@gmail.com>
2021-11-14 22:54:13 -0500

    help

--- app.js
+++ app.js
@@ -18,14 +18,6 @@ hamburger.addEventListener('click', () => {
     }
 });
 
-window.addEventListener("scroll", preventMotion, false);
-window.addEventListener("touchmove", preventMotion, false);
 
-function preventMotion(event)
-{
-    window.scrollTo(0, 0);
-    event.preventDefault();
-    event.stopPropagation();
-}
 
 
